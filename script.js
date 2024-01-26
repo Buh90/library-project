4f9cc3fe9c6091b6773fbd625cf8b09450606abe
Buh90 <83811198+Buh90@users.noreply.github.com>
2024-01-26 19:45:24 +0100

    Start project

---
+++ script.js
@@ -0,0 +1,6 @@
+const openModalBtn = document.querySelector(".plus-card");
+const modal = document.querySelector("dialog");
+
+openModalBtn.addEventListener("click", () => {
+  modal.showModal();
+});
